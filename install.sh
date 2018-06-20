632b50534f2746ce7a55269a6a9815c89c6177e5
josedelabastida <josedelabastida@gmail.com>
2018-06-20 15:26:58 +0800

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-HOSTNAME=$hostname
-sudo chmod 777 /etc/hosts
-sudo echo "127.0.1.1 ${HOSTNAME}" >> /etc/hosts
-sudo chmod 644 /etc/hosts
-sudo apt-get update && sudo apt-get install -y default-jre
