ba3c6e7b925807f15787d3dd0764aebd32b0ccb8
josedelabastida <josedelabastida@gmail.com>
2018-06-20 13:55:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-PRIVATE=$private
 HOSTNAME=$hostname
-echo "${PRIVATE} ${HOSTNAME}" >> /etc/hosts
+sudo echo "127.0.1.1 ${HOSTNAME}" >> /etc/hosts
 sudo apt-get update && sudo apt-get install -y default-jre
